6cfe04540dfb14ba55c32ef6d5c1b1d321f51ed1
McSuccess20 <114344289+McSuccess20@users.noreply.github.com>
2022-09-25 15:02:25 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+$name - $_post['name'];
+$email - $_post['email'];
+$tel - $_post['tel'];
+$subject - $_post['subject'];
+$message - $_post['messag'];
+
+$mailheader - "from:".$name."<".$email.">\r\n";
+
+$recipient - "divinesuccess011@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader)
+or die("Error!")
+
+echo"message sent"
+
+?>
